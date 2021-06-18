e0865fdff93f70757e98b9a0584e3625caccb25e
Ajay1910-svg <84716743+Ajay1910-svg@users.noreply.github.com>
2021-06-18 15:04:08 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,17 +1,5 @@
 /* Enter your solutions in this file */
-#include <stdio.h>
-#include <assert.h>
 
-int max(int [], int);
-
-int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert (max(x, 7) == 10);
-
-  int y[] = {5};
-  assert (max(y, 1) == 5);
-  printf("Max: passed\n");
-  }
 max(int x[],int k)
 {
   int t,i;
@@ -23,3 +11,5 @@ max(int x[],int k)
   }
   return(t);
 }
+
+
